812506cdd34fa7f1d1af1b241952a0994df42299
nossika <476492581@qq.com>
2016-11-09 17:54:29 +0800

    ejs

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const koa_static = require('koa-static');
 const koa_router = require('koa-router');
 const api = koa_router();
 const app = koa();
+const ejs = require('ejs');
+const fs = require('fs');
+
+
 
 app.use(koa_static(__dirname + '/static'));
 app.use(api.routes()).use(api.allowedMethods());
 
 api.get('/', function* (){
-    this.body = 'aaa';
+    let html = ejs.render(fs.readFileSync(__dirname + '/views/index.ejs','utf-8'),{
+        aaa: 111154,
+        filename: __dirname + '/views/parts/'
+    });
+    this.body = html;
 });
+console.log(ejs)
 
 app.listen(7869);
