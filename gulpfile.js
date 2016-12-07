af7e7d1e1c38e9f3a3fce541b10f4cac759f7b3a
nossika <476492581@qq.com>
2016-12-07 14:39:56 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 const gulp = require('gulp');
-// const uglify = require('gulp-uglify');
-// const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const less = require('gulp-less');
 
-gulp.task('min_js', () => {
-    // gulp.src('./static/js/*.js')
-    //     .pipe(concat('compressed.js'))
-    //     .pipe(uglify())
-    //     .pipe(gulp.dest('./ddd'));
-    gulp.src('./static/js/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('./ddd'));
+gulp.task('package_js', () => {
+    gulp.src(['static/js/util.js', 'static/js/utils/*.js'])
+        .pipe(babel({
+            presets: ['es2015'],
+            // plugins: ['transform-runtime'],
+        }))
+        .pipe(concat('compressed.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('static/build'))
 });
-gulp.task('default', ['min_js'], () => {
-    console.log('success')
+
+gulp.task('default', ['package_js'], () => {
+    console.log('done');
 });
